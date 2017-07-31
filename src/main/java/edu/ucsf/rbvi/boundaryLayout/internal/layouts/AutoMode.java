4a64008b2e9c5afcc7a948e5d90f7644fd21a766
Allison Yih <allieyih@gmail.com>
2017-07-31 12:25:18 -0700

    getting list of categories

--- src/main/java/edu/ucsf/rbvi/boundaryLayout/internal/layouts/AutoMode.java
+++ src/main/java/edu/ucsf/rbvi/boundaryLayout/internal/layouts/AutoMode.java
@@ -1,7 +1,9 @@
 package edu.ucsf.rbvi.boundaryLayout.internal.layouts;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import org.cytoscape.view.presentation.annotations.AnnotationFactory;
@@ -13,6 +15,7 @@ import org.cytoscape.view.presentation.property.BasicVisualLexicon;
 
 import prefuse.util.force.ForceSimulator;
 
+import org.cytoscape.model.CyNetwork;
 import org.cytoscape.model.CyNode;
 import org.cytoscape.service.util.CyServiceRegistrar;
 import org.cytoscape.view.model.CyNetworkView;
@@ -26,9 +29,27 @@ public class AutoMode { //AutoMode class called to create all the annotations
 	public static Map<Object, ShapeAnnotation> createAnnotations(CyNetworkView cNV, 
 					                                                     Set<View<CyNode>> nodesToLayout, 
 						                                                   String categoryColumn) {	
+		
 		//to do 
-		// 1. Get the list of categories and required space for each category
+		// 1. Get the list of categories and required space for each category	
+		CyNetwork network = cNV.getModel();
+		
+		Map<Object, List<View<CyNode>>> categories = new HashMap<Object,List<View<CyNode>>>();
+		
+		for (View<CyNode> nv: nodesToLayout) {
+			CyNode node = nv.getModel();
+			
+			Object cat = network.getRow(node).getRaw(categoryColumn);
+			if(!categories.containsKey(cat)) 
+				categories.put(cat, new ArrayList<View<CyNode>>());
+			categories.get(cat).add(nv);
+		}
+		
+		double spacing;
+		
 		// 2. For each category create a rounded rectangle annotation of the correct size
 		return null;
 	}
+	
+	
 }
