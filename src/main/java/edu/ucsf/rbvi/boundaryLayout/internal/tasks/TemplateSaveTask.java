62a1b6301d82559fbb648e879a311ee50a40b057
moelattma <17elattmam00@hillsdaleknights.org>
2017-08-28 15:06:49 -0700

    Finished saving to a unique text file implementation

--- src/main/java/edu/ucsf/rbvi/boundaryLayout/internal/tasks/TemplateSaveTask.java
+++ src/main/java/edu/ucsf/rbvi/boundaryLayout/internal/tasks/TemplateSaveTask.java
@@ -1,9 +1,12 @@
 package edu.ucsf.rbvi.boundaryLayout.internal.tasks;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,9 +25,6 @@ public class TemplateSaveTask extends AbstractTask {
 
 	@Tunable(description="File containing templates", params="input=true;fileCategory=unspecified")
 	public File bTemplates;
-
-	@Tunable(description="Template name")
-	public String templateName = "";
 
 	public TemplateSaveTask(CyServiceRegistrar registrar, CyNetworkView networkView) {
 		super();
@@ -55,16 +55,13 @@ public class TemplateSaveTask extends AbstractTask {
 		}
 		else return null;
 	}
-
+	
 	private void saveToFile(List<ShapeAnnotation> shapeAnnotations) throws IOException {
 		if(!bTemplates.exists())
 			bTemplates.createNewFile();
 		BufferedWriter bTempWriter = new BufferedWriter(new FileWriter(bTemplates.getAbsolutePath()));
 		char shapeAnnotationIndex = 'a';
-		
-		/*ADD: Overwriting a previous save*/
-		
-		bTempWriter.write("\"" + templateName + "\"");
+		bTempWriter.write(bTemplates.getName());
 		for(ShapeAnnotation shapeAnnotation : shapeAnnotations) { 
 			bTempWriter.newLine();
 			bTempWriter.write("   " + (shapeAnnotationIndex++) + ") " + shapeAnnotation.getArgMap());
@@ -76,4 +73,59 @@ public class TemplateSaveTask extends AbstractTask {
 					"[" + e.getMessage()+ "]");
 		}
 	}
+
+	/*private void saveToFile(List<ShapeAnnotation> shapeAnnotations) throws IOException {
+		templateName = "\"" + templateName + "\"";
+		if(!bTemplates.exists())
+			bTemplates.createNewFile();
+		BufferedWriter bTempWriter = new BufferedWriter(new FileWriter(bTemplates.getAbsolutePath()));
+
+		List<String> tempLines = new ArrayList<>();
+		boolean isOverWritten = checkOverwrite(tempLines);
+		File newBTemplates = new File("temp.txt");
+		newBTemplates.renameTo(bTemplates);
+		bTemplates.delete();
+		if(tempLines.get(0).equals(""))
+			tempLines.add(templateName + ",");
+
+		/*Need to add: Method of overwriting a previous save -- check if the name has already been used -- 
+		 * should also store the list of already saved templates in the text file to easily see.*/
+
+	/*	char shapeAnnotationIndex = 'a';
+		bTempWriter.newLine();
+		bTempWriter.write(templateName);
+		for(ShapeAnnotation shapeAnnotation : shapeAnnotations) { 
+			bTempWriter.newLine();
+			bTempWriter.write("   " + (shapeAnnotationIndex++) + ") " + shapeAnnotation.getArgMap());
+		}
+		try {
+			bTempWriter.close();
+		} catch (IOException e) {
+			throw new IOException("Problems writing to stream: " + bTempWriter.toString() + 
+					"[" + e.getMessage()+ "]");
+		}
+	}
+
+	private static List<String> getFileLines(File templateFile) throws IOException {
+		Scanner bTempScanner = new Scanner(new BufferedReader(new BufferedReader(new 
+				FileReader(templateFile.getAbsolutePath()))));
+		List<String> templateLines = new ArrayList<>();
+		while(bTempScanner.hasNext()) 
+			templateLines.add(bTempScanner.nextLine());
+		bTempScanner.close();
+		return templateLines;
+	}
+
+	private boolean checkOverwrite(List<String> templateLines) throws IOException {
+		templateLines = getFileLines(bTemplates);
+		if(templateLines.get(0).contains(templateName)) 
+			for(int lineIndex = 0; lineIndex < templateLines.size(); lineIndex++) 
+					if(templateLines.get(lineIndex).charAt(0) == '\"' && 
+				templateLines.get(lineIndex).contains(templateName)) {
+					while(lineIndex < templateLines.size() && templateLines.get(lineIndex).charAt(0) != '\"')
+						templateLines.remove(lineIndex);
+					return true;
+				}
+		return false;
+	}*/
 }
