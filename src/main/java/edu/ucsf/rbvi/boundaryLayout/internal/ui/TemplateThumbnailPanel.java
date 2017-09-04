bdaf7c0bd49593691b29968084457f332a8985ba
moelattma <17elattmam00@hillsdaleknights.org>
2017-09-04 15:38:57 -0700

    One possible implementation of the thumbnail interface. ImageIcon does not work -- all white.

--- src/main/java/edu/ucsf/rbvi/boundaryLayout/internal/ui/TemplateThumbnailPanel.java
+++ src/main/java/edu/ucsf/rbvi/boundaryLayout/internal/ui/TemplateThumbnailPanel.java
@@ -6,14 +6,18 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.GridLayout;
 import java.awt.Image;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.Box;
 import javax.swing.BoxLayout;
+import javax.swing.DefaultListModel;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JWindow;
@@ -32,7 +36,8 @@ public class TemplateThumbnailPanel extends JPanel implements CytoPanelComponent
 	private TemplateManager manager;
 	private ImageIcon templateIcon;
 	private JScrollPane scrollPane;
-	
+	private JList<JButton> templateThumbnails;
+
 	public TemplateThumbnailPanel() {
 		this(null, null);
 	}
@@ -46,7 +51,6 @@ public class TemplateThumbnailPanel extends JPanel implements CytoPanelComponent
 
 		// This will contain all of our template buttons
 		scrollPane = new JScrollPane();
-		scrollPane.setLayout(new ScrollPaneLayout());
 		this.add(scrollPane);
 
 		// Draw our panel
@@ -54,14 +58,21 @@ public class TemplateThumbnailPanel extends JPanel implements CytoPanelComponent
 	}
 
 	public void updatePanel() {
-		for (String template : manager.getTemplateNames()) {
-			Image thumbnail = manager.getThumbnail(template);
-			JButton templateButton = new JButton(template, new ImageIcon(thumbnail));
-			templateButton.setActionCommand(template);
-			templateButton.addActionListener(this);
-			scrollPane.add(Box.createRigidArea(new Dimension(0,5)));
-			scrollPane.add(templateButton);
+		JPanel buttonsPanel = new JPanel(new GridLayout(0, 1));
+		List<JButton> templateButtons = new ArrayList<>();
+		for(String templateName : manager.getTemplateNames()) {
+			Image thumbnail = manager.getThumbnail(templateName);
+			JButton templateButton = new JButton(templateName, new ImageIcon(thumbnail));
+			templateButtons.add(templateButton);
+			buttonsPanel.add(templateButton);
+			templateButton.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent event) {
+					//implement to apply thumbnail
+				}
+			});
 		}
+		scrollPane.setViewportView(buttonsPanel);
 	}
 
 	@Override
